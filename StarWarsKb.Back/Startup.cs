c00164f61482304899fa52c2c74fb1ca2fb06dc6
Ingafen <ingafen@gmail.com>
2022-05-19 16:04:22 +0500

    Move migrations running to Back App startup

--- StarWarsKb.Back/Startup.cs
+++ StarWarsKb.Back/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using StarWarsKb.Back.Model;
 using StarWarsKb.Infrastructure.Model;
 using StarWarsKb.Infrastructure.Services;
@@ -41,10 +33,7 @@ namespace StarWarsKb.Back
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
